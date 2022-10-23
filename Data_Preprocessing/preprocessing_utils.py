12f14336e02760cb3bc368816eeeb93a53e1abda
Diksha Sethi <dikshasethi251@gmail.com>
2022-10-23 12:19:22 +0530

    Added few preprocessing func

---
+++ Data_Preprocessing/preprocessing_utils.py
@@ -0,0 +1,16 @@
+from nltk.tokenize import TweetTokenizer
+import re
+
+def tokenize(text: str) -> list[str]:
+    tk = TweetTokenizer()
+    tokenized = tk.tokenize(text)
+    return tokenized
+
+def replace_username(post: str) -> str:
+    post = re.sub("@[A-Za-z0-9_]+", '<username>', post)
+    return post
+
+def replace_urls(post: str) -> str:
+    regex = "(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})"
+    post = re.sub(regex, "<url>", post)
+    return post
